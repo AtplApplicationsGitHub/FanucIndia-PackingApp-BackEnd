6d70900553f3438feb58af9460e68532e8230287
Anirudh030703 <anirudh.s@adventsys.co>
2025-09-18 22:55:06 +0530

    18th Sep Backend 10:55PM

--- src/main.ts
+++ src/main.ts
@@ -11,48 +11,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: false });
   const configService = app.get(ConfigService);
 
-  const httpsKeyPath = configService.get<string>('SSL_KEY_PATH');
-  console.log(httpsKeyPath);
-  const httpsCertPath = configService.get<string>('SSL_CERT_PATH');
-  console.log(httpsCertPath);
-  const isSSL = !!(httpsKeyPath && httpsCertPath);
-
-  let listenApp = app;
-
-  // if (isSSL) {
-  //   const httpsOptions = {
-  //     key: fs.readFileSync(httpsKeyPath!),
-  //     cert: fs.readFileSync(httpsCertPath!),
-  //   };
-  //   await app.close();
-  //   listenApp = await NestFactory.create(AppModule, { httpsOptions, logger: false });
-  // }
-  if (isSSL) {
-    // Read the key and certificate files into variables
-    const privateKey = fs.readFileSync(httpsKeyPath!);
-    const certificate = fs.readFileSync(httpsCertPath!);
-
-    // Log the contents to the console
-    // Using .toString() to see the text content instead of a buffer
-    console.log("--- HTTPS Key ---");
-    console.log(privateKey.toString()); 
-
-    console.log("--- HTTPS Certificate ---");
-    console.log(certificate.toString());
-
-    const httpsOptions = {
-        key: privateKey,
-        cert: certificate,
-    };
-
-    await app.close();
-    listenApp = await NestFactory.create(AppModule, { httpsOptions, logger: false });
-}
-
   const pinoAdapter = new PinoLogger();
-  listenApp.useLogger(pinoAdapter);
+  app.useLogger(pinoAdapter);
 
-  listenApp.useGlobalPipes(
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
@@ -66,18 +28,18 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(listenApp, swaggerConfig);
-  SwaggerModule.setup('api', listenApp, document);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
-  listenApp.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
-  listenApp.enableCors({
+  app.enableCors({
     origin: ['http://localhost:3000', 'https://fanuc.goval.app:444'],
     credentials: true,
   });
 
   const port = process.env.PORT || 3011;
-  await listenApp.listen(port, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
   pinoAdapter.log(`Application is listening on port ${port}`, 'bootstrap');
   pinoAdapter.log('Application started', 'bootstrap');
 }
