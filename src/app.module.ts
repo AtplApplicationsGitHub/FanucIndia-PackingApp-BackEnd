917d994ac0d1bd93006d3834261a9f23a0f71d02
Anirudh030703 <anirudh.s@adventsys.co>
2026-03-17 21:11:10 +0530

    CRON JOB AUTOMATION ERP DATA fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,9 +25,10 @@ import { SoChatModule } from './modules/so-chat/so-chat.module';
 import { SoNotificationsModule } from './modules/so-notifications/so-notifications.module';
 import { AppUpdateModule } from './modules/app-update/app-update.module';
 import { ReportsModule } from './modules/reports/reports.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, SalesOrderModule, AuthModule, LookupModule, SalesCrudModule, AdminOrderModule, UserModule, ErpMaterialDataModule, ErpMaterialFileModule, SftpModule, UserDashboardModule, ErpMaterialImporterModule, DispatchModule, SoSearchModule, FgDashboardModule, FgStorageModule, SoArchiveModule, DashboardModule, VehicleEntryModule, SoChatModule, SoNotificationsModule, AppUpdateModule, ReportsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, SalesOrderModule, AuthModule, LookupModule, SalesCrudModule, AdminOrderModule, UserModule, ErpMaterialDataModule, ErpMaterialFileModule, SftpModule, UserDashboardModule, ErpMaterialImporterModule, DispatchModule, SoSearchModule, FgDashboardModule, FgStorageModule, SoArchiveModule, DashboardModule, VehicleEntryModule, SoChatModule, SoNotificationsModule, AppUpdateModule, ReportsModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
